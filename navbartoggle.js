45388d3fca20b8d3cac8a73cf03a142664f2dde3
AoDOsiris <Combs.Derek@gmail.com>
2025-07-26 16:45:20 -0700

    Revamped

---
+++ navbartoggle.js
@@ -0,0 +1,7 @@
+const navbarToggle = document.getElementById('.navbar-toggle');
+const navbarMenu = document.getElementById('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => { 
+    navbarToggle.classList.toggle('active'); 
+    navbarMenu.classList.toggle('active');
+})
