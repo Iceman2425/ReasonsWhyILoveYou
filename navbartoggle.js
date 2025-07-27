7036f512b8748e14246e81824c79b68d14c475cf
AoDOsiris <Combs.Derek@gmail.com>
2025-07-27 16:28:39 -0700

    Responsive Nav?

--- navbartoggle.js
+++
@@ -1,7 +0,0 @@
-const navbarToggle = document.getElementById('.navbar-toggle');
-const navbarMenu = document.getElementById('.navbar-menu');
-
-navbarToggle.addEventListener('click', () => { 
-    navbarToggle.classList.toggle('active'); 
-    navbarMenu.classList.toggle('active');
-})
